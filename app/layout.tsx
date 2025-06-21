6e624a7c76006385d0f27c9d6565cff41d64d75f
Sarah <109141163+AutumnsCode@users.noreply.github.com>
2025-06-21 08:40:40 +0200

    initial project and add layout page to the sign up /in  page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
 import "./globals.css";
+import { Poppins } from "next/font/google"
 
 
 const poppins = Poppins({
-
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins'
 })
 
 export const metadata: Metadata = {
@@ -20,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} font-poppins antialiased`}
       >
         {children}
       </body>
