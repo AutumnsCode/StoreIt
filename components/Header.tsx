0f0daba2b5eb8f2cdcc3c13fc0b6907c57326e2f
Sarah <109141163+AutumnsCode@users.noreply.github.com>
2025-06-22 14:05:59 +0200

    layout, sidebar, header

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Search from './Search'
+import FileUploader from './FileUploader'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+const Header = ({
+  userId,
+  accountId
+}: {
+    userId: string
+    accountId: string
+}) => {
+  return (
+    <header className='header'>
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader />
+        <form>
+          <Button type='submit' className='sign-out-button'>
+            <Image
+              src="/assets/icons/logout.svg"
+              alt='logo'
+              width={24}
+              height={24}
+              className='w-6'
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,49 +1,48 @@
-"use server"
+"use server";
 
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
-import { appwriteConfig } from "./config";
-import { Account, Avatars, Client, Databases, Storage } from "node-appwrite"
 
 export const createSessionClient = async () => {
-    const client = new Client()
-        .setEndpoint(appwriteConfig.endpointUrl)
-        .setProject(appwriteConfig.projectId)
-    
-    const session = (await cookies()).get('appwrite-session')
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
 
-    if (!session || !session.value) throw new Error('No session')
-    
-    client.setSession(session.value)
+  const session = (await cookies()).get("appwrite-session");
 
-    return {
-        get account() {
-            return new Account(client)
-        },
-        get databases() {
-            return new Databases(client)
-        }
-    }
-}
+  if (!session || !session.value) throw new Error("No session");
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
 
 export const createAdminClient = async () => {
-    const client = new Client()
-        .setEndpoint(appwriteConfig.endpointUrl)
-        .setProject(appwriteConfig.projectId)
-        .setKey(appwriteConfig.secretKey)
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
 
-
-    return {
-        get account() {
-            return new Account(client)
-        },
-        get databases() {
-            return new Databases(client)
-        },
-        get storage() {
-            return new Storage(client)
-        },
-        get avatars() {
-            return new Avatars(client)
-        }
-    }
-}
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+}; 
