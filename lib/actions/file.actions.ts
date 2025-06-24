db1eaa9596adbcfb1b454865d27cb85bff5bd5de
Sarah <109141163+AutumnsCode@users.noreply.github.com>
2025-06-24 10:07:48 +0200

    sort files to the correct page

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -54,7 +54,7 @@ export const uploadFile = async({
   }
 }
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[]) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
@@ -62,18 +62,20 @@ const createQueries = (currentUser: Models.Document) => {
     ])
   ]
 
+  if (types.length > 0) queries.push(Query.equal("type", types))
+
   // Todo: Search, sort, limits...
 
   return queries
 }
 
-export const getFiles = async () => {
+export const getFiles = async ({types =[]}: GetFilesProps) => {
   const { databases } = await createAdminClient()
   try {
     const currentUser = await getCurrentUser()
     if (!currentUser) throw new Error("User not found")
     
-    const queries = createQueries(currentUser)
+    const queries = createQueries(currentUser, types)
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
